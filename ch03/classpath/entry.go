c55c3cb7f0eab7be6c1276d35badb50fa3c397c3
wangkefan <wangkefan@baidu.com>
2018-04-02 20:30:06 +0800

    jvmgo

---
+++ ch03/classpath/entry.go
@@ -0,0 +1,31 @@
+package classpath
+
+import (
+	"os"
+	"strings"
+)
+
+const (
+	pathListSeparator = string(os.PathSeparator)
+)
+
+type Entry interface {
+	readClass(className string) ([]byte, Entry, error)
+	String() string
+}
+
+func newEntry(path string) Entry {
+	if strings.Contains(path, pathListSeparator) {
+		return newCompositeEntry(path)
+	}
+	if strings.HasSuffix(path, "*") {
+		return newWildcardEntry(path)
+	}
+	if strings.HasSuffix(path, ".jar") || strings.HasSuffix(path, ".JAR") {
+		return newZipEntry(path)
+	}
+	if strings.HasSuffix(path, ".zip") || strings.HasSuffix(path, ".ZIP") {
+		return newZipEntry(path)
+	}
+	return newDirEntry(path)
+}
