611b3b2fc65ed81886fcd4799638c096bd42cd13
wangkefan <wangkefan@baidu.com>
2018-04-08 10:19:56 +0800

    运行时数据区

--- ch04/rtda/frame.go
+++ ch04/rtda/frame.go
@@ -1,11 +1,22 @@
 package rtda
 
 type Frame struct {
-	lower        *Frame	// 链表模拟栈帧
-	localVars    LocalVars	// 保存局部变量指针
-	operandStack *OperandStack	// 保存操作数栈指针
+	lower        *Frame        // 链表模拟栈帧
+	localVars    LocalVars     // 保存局部变量指针
+	operandStack *OperandStack // 保存操作数栈指针
 }
 
-func newFrame(maxLocals, maxStack uint) *Frame {
+func NewFrame(maxLocals, maxStack uint) *Frame {
+	return &Frame{
+		localVars: newLocalVars(maxLocals),
+		operandStack: newOperandStack(maxStack),
+	}
+}
 
+func (self *Frame) LocalVars() LocalVars {
+	return self.localVars
+}
+
+func (self *Frame) OperandStack() *OperandStack {
+	return self.operandStack
 }
