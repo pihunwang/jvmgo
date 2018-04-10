e170d1cf57745dc1a24cbc567221bada74dc1665
wangkefan <lovewangkefan@163.com>
2018-04-10 20:25:35 +0800

    classloader

--- class/rtda/heap/class_loader.go
+++ class/rtda/heap/class_loader.go
@@ -12,9 +12,23 @@ type ClassLoader struct {
 }
 
 func NewClassLoader(cp *classpath.Classpath) *ClassLoader {
-
+	return &ClassLoader{
+		cp:cp,
+		classMap:make(map[string]*Class),
+	}
 }
 
 func (self *ClassLoader) LoadClass(name string) *Class {
+	if class, ok := self.classMap[name]; ok {
+		return class
+	}
+	return self.loadNonArrayClass(name)
+}
 
+func (self *ClassLoader) loadNonArrayClass(name string) *Class {
+	data, entry := self.readClass(name)
+	class := self.defineClass(data)
+	link(class)
+	fmt.Printf("[Loaded %s from %s]\n", name, entry)
+	return class
 }
