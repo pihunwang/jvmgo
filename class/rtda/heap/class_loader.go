4f80e690a522590a0a2bb50431447529be4c1b3c
wangkefan <lovewangkefan@163.com>
2018-04-11 20:47:35 +0800

    类加载器 classloader

--- class/rtda/heap/class_loader.go
+++ class/rtda/heap/class_loader.go
@@ -26,9 +26,68 @@ func (self *ClassLoader) LoadClass(name string) *Class {
 }
 
 func (self *ClassLoader) loadNonArrayClass(name string) *Class {
+	// 找到class文件并读取到内存
 	data, entry := self.readClass(name)
+	// 解析class文件,生成虚拟机可用的类数据,放入方法区
 	class := self.defineClass(data)
+	// 链接
 	link(class)
 	fmt.Printf("[Loaded %s from %s]\n", name, entry)
 	return class
+}
+
+func (self *ClassLoader) readClass(name string) ([]byte, classpath.Entry) {
+	data, entry, err := self.cp.ReadClass(name)
+	if err != nil {
+		panic("java.lang.ClassNotFoundException: " + name)
+	}
+	return data, entry
+}
+
+func (self *ClassLoader) defineClass(data []byte) *Class {
+	class := parseClass(data)
+	class.loader = self
+	resolveSuperClass(class)
+	resolveInterfaces(class)
+	self.classMap[class.name] = class
+	return class
+}
+
+func parseClass(data []byte) *Class {
+	cf, err := classfile.Parse(data)
+	if err != nil {
+		panic("java.lang.ClassFormatError")
+	}
+	return newClass(cf)
+}
+
+func resolveSuperClass(class *Class) {
+	if class.name != "java/lang/Object" {
+		class.superClass = class.loader.LoadClass(class.superClassName)
+	}
+}
+
+func resolveInterfaces(class *Class) {
+	interfaceCount := len(class.interfaceNames)
+	if interfaceCount > 0 {
+		class.interfaces = make([]*Class, interfaceCount)
+		for i, interfaceName := range class.interfaceNames {
+			class.interfaces[i] = class.loader.LoadClass(interfaceName)
+		}
+	}
+}
+
+func link(class *Class) {
+	// 验证
+	verify(class)
+	// 准备
+	prepare(class)
+}
+
+func verify(class *Class) {
+	//todo
+}
+
+func prepare(class *Class) {
+
 }
